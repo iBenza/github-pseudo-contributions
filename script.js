1a7ad0dfa0065b3eac2004354bc4752590d8fc1b
Shin'ya Ueoka <ibenza@i-beam.org>
2015-05-25 23:12:40 +0900

    Make to draw by dragging

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
   colors = ['#eeeeee', '#d6e685', '#8cc665', '#44a340', '#1e6823'];
   days = document.getElementsByClassName('day');
   Array.prototype.forEach.call(days, function(element) {
-    element.addEventListener('click', function(event){
-      event.stopPropagation()
+    element.addEventListener('click', function(event) {
+      event.stopPropagation();
+    });
+    element.addEventListener('mouseenter', function(event) {
       currentColor = element.getAttribute('fill');
-      nextIndex = (colors.indexOf(currentColor) + 1) % colors.length;
-      element.setAttribute('fill', colors[nextIndex]);
+      if (event.buttons & 1 && !event.shiftKey) {
+        nextIndex = Math.min(colors.indexOf(currentColor) + 1, colors.length - 1);
+        element.setAttribute('fill', colors[nextIndex]);
+      } else if (event.buttons & 1 && event.shiftKey) {
+        nextIndex = Math.max(colors.indexOf(currentColor) - 1, 0);
+        element.setAttribute('fill', colors[nextIndex]);
+      }
     });
   });
 })();
